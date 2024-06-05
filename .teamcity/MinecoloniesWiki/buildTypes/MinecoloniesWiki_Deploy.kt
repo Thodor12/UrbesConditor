b79ad43cd7b9b4d2a7ef267812bae261da1d4ec2
Thom van den Akker <thodor20@gmail.com>
2024-06-05 10:54:28 +0200

    Fix kubectl installation

--- .teamcity/MinecoloniesWiki/buildTypes/MinecoloniesWiki_Deploy.kt
+++ .teamcity/MinecoloniesWiki/buildTypes/MinecoloniesWiki_Deploy.kt
@@ -36,12 +36,14 @@ object MinecoloniesWiki_Deploy : BuildType({
                     
                     # prereq packages
                     sudo apt-get update
-                	sudo apt-get install -y wget ca-certificates gnupg2
+                	sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
                 
                 	# add repo and signing key
                     sudo mkdir -p /etc/apt/keyrings
-                    curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
-                	echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+                    curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+                    sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+                	echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+                    sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
                     sudo apt-get update
                 
                 	# install kubectl
